4131d161290a809985f7010f3bffb77bf14ebce8
Burak Kocak <burakkocak884@ghotmail.com>
2019-03-24 12:13:45 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,13 @@
 require 'pry'
 class Dog
-@@all =[]
-attr_accessor :name, :breed
-attr_reader :id
-		def initialize(name:, breed:, id:  nil)
+
+attr_accessor :id, :name, :breed
+
+		def initialize(name:, breed:, id: nil)
 			@name  = name
 			@breed = breed
 			@id = id 
-      @@all << self
+      
    
     
 		end
@@ -36,7 +36,7 @@ attr_reader :id
    		     INSERT INTO dogs (name, breed) VALUES (? , ?)
       		SQL
       		DB[:conn].execute(sql, self.name, self.breed)
-            #@id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
              #binding.pry
              self
            
@@ -50,6 +50,8 @@ attr_reader :id
                dog
           end
 
+
+
            def self.find_by_id(dog)
              #binding.pry
               sql = <<-SQL 
@@ -57,50 +59,48 @@ attr_reader :id
                SQL
               dogs =  DB[:conn].execute(sql, dog)[0]
               
-              Dog.new(:id => dogs[0], :name => dogs[1], :breed => dogs[2])
+              Dog.new( :name => dogs[1], :breed => dogs[2],:id => dogs[0])
              #binding.pry
            end
+
+
             def self.new_from_db(dog)
                 #binding.pry
-                new_dog = Dog.new(:id => dog[0], :name => dog[1], :breed => dog[2])
-                binding.pry
+                new_dog = Dog.new(:name => dog[1], :breed => dog[2], :id => dog[0])
+                #binding.pry
               
              end
 
+
              def self.find_by_name(dog)
                 sql = <<-SQL 
-              SELECT * FROM dogs where name = ? 
-               SQL
-              dogs =  DB[:conn].execute(sql, dog).flatten
-             Dog.new(:id => dogs[0], :name => dogs[1], :breed => dogs[2])
+                SELECT * FROM dogs where name = ? 
+                SQL
+                dogs =  DB[:conn].execute(sql, dog).flatten
+                Dog.new(:name => dogs[1], :breed => dogs[2],:id => dogs[0])
              end
 
 
             def update
-           
-               sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
-               # binding.pry
-                DB[:conn].execute(sql)
-           end
-          
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+        DB[:conn].execute(sql, self.name, self.breed, self.id)
+      end
           
           def self.find_or_create_by(name:, breed:)
-            sql = <<-SQL 
-              SELECT * FROM dogs where name = ? AND breed =? LIMIT 1
-               SQL
-              #binding.pry
-              dogs =  DB[:conn].execute(sql, name, breed)
-            #binding.pry
-              if dogs.empty?
-
-                n_dog = self.create(:name => name, :breed => breed)
-
-              else
-                n_dog = self.new_from_db(dogs[0])
-              end
-              n_dog
+                sql = <<-SQL 
+                  SELECT * FROM dogs where name = ? AND breed =?
+                   SQL
+                  #binding.pry
+                  dogs =  DB[:conn].execute(sql, name, breed).first
+              #  binding.pry
+                  if dogs
+                      n_dog = self.find_by_id(dogs[0])
+                    else
+                       n_dog = create(:name => name, :breed => breed)
+                   end
+                  n_dog
          
-               end
+            end
        
 
 
